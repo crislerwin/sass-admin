19005336cfbdb71ab5db59c99dc3d914e8ec5966
Ubuntu <crisler@ubuntuVM.zicgg1scgt2uhgpejip2ferf5c.cx.internal.cloudapp.net>
2021-07-05 11:02:06 +0000

    chore: first commit with typescript and sass

---
+++ src/utils/sample-data.ts
@@ -0,0 +1,10 @@
+export type User = {
+  id: number;
+  name: string;
+};
+export const sampleUserData: User[] = [
+  { id: 101, name: "Alice" },
+  { id: 102, name: "Bob" },
+  { id: 103, name: "Caroline" },
+  { id: 104, name: "Dave" },
+];
