97a63366a751b04012765a59caf4a6fa8c63df19
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-07-24 21:58:59 -0300

    feat: added react-icons

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,15 @@
+import {
+  FaChartBar,
+  FaFolderOpen,
+  FaFont,
+  FaHome,
+  FaInfo,
+  FaLaptop,
+  FaList,
+  FaMapMarked,
+  FaPowerOff,
+  FaTable,
+} from "react-icons/fa";
 export const Sidebar: React.FC<{}> = () => {
   return (
     <>
@@ -6,55 +18,73 @@ export const Sidebar: React.FC<{}> = () => {
         <ul>
           <li>
             <a href="#">
-              <i className="fa fa-home fa-2x"></i>
+              <i className="fa  fa-2x">
+                <FaHome />
+              </i>
               <span className="nav-text">Dashboard</span>
             </a>
           </li>
           <li className="has-subnav">
             <a href="#">
-              <i className="fa fa-laptop fa-2x"></i>
+              <i className="fa  fa-2x">
+                <FaLaptop />
+              </i>
               <span className="nav-text">Componentes</span>
             </a>
           </li>
           <li className="has-subnav">
             <a href="#">
-              <i className="fa fa-list fa-2x"></i>
+              <i className="fa  fa-2x">
+                <FaList />
+              </i>
               <span className="nav-text">Formulários</span>
             </a>
           </li>
           <li className="has-subnav">
             <a href="#">
-              <i className="fa fa-folder-open fa-2x"></i>
+              <i className="fa  fa-2x">
+                <FaFolderOpen />
+              </i>
               <span className="nav-text">Paginas</span>
             </a>
           </li>
           <li>
             <a href="#">
-              <i className="fa fa-bar-chart-o fa-2x"></i>
+              <i className="fa  fa-2x">
+                <FaChartBar />
+              </i>
               <span className="nav-text">Graficos</span>
             </a>
           </li>
           <li>
             <a href="#">
-              <i className="fa fa-font fa-2x"></i>
+              <i className="fa  fa-2x">
+                <FaFont />
+              </i>
               <span className="nav-text">Anotações</span>
             </a>
           </li>
           <li>
             <a href="#">
-              <i className="fa fa-table fa-2x"></i>
+              <i className="fa  fa-2x">
+                <FaTable />
+              </i>
               <span className="nav-text">Tabelas</span>
             </a>
           </li>
           <li>
             <a href="#">
-              <i className="fa fa-map-marker fa-2x"></i>
+              <i className="fa  fa-2x">
+                <FaMapMarked />
+              </i>
               <span className="nav-text">Mapas</span>
             </a>
           </li>
           <li>
             <a href="#">
-              <i className="fa fa-info fa-2x"></i>
+              <i className="fa fa-2x">
+                <FaInfo />
+              </i>
               <span className="nav-text">Documentação</span>
             </a>
           </li>
@@ -63,7 +93,9 @@ export const Sidebar: React.FC<{}> = () => {
         <ul className="logout">
           <li>
             <a href="#">
-              <i className="fa fa-power-off fa-2x"></i>
+              <i className="fa fa-2x">
+                <FaPowerOff />
+              </i>
               <span className="nav-text">Sair</span>
             </a>
           </li>
