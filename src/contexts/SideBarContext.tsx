894f57beae92fd6c0e2156ed0c9995d053e50bf2
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-03 20:33:01 -0300

    ✨ create a jwt auth

--- src/contexts/SideBarContext.tsx
+++ src/contexts/SideBarContext.tsx
@@ -1,17 +1,64 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { setCookie, parseCookies } from "nookies";
+import Router from "next/router";
 
-export const SideBarContext = createContext(null);
+import { recoverUserInformation, signInRequest } from "../services/auth";
+import { api } from "../services/api";
 
-export const SideProvider: React.FC<{}> = ({ children }) => {
-  const [item, setItem] = useState<[]>();
-  return (
-    <SideBarContext.Provider
-      value={{
-        item,
-        setItem,
-      }}
-    >
-      {children}
-    </SideBarContext.Provider>
-  );
+type User = {
+  name: string;
+  email: string;
+  avatar_url: string;
 };
+
+type SignInData = {
+  email: string;
+  password: string;
+};
+
+type AuthContextType = {
+  isAuthenticated: boolean;
+  user: User;
+  signIn: (data: SignInData) => Promise<void>;
+};
+
+export const SideContext = createContext({} as AuthContextType);
+
+export function SideProvider({ children }: any) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      recoverUserInformation().then((response) => {
+        setUser(response.user);
+      });
+    }
+  }, []);
+
+  async function signIn({ email, password }: SignInData) {
+    const { token, user } = await signInRequest({
+      email,
+      password,
+    });
+
+    setCookie(undefined, "nextauth.token", token, {
+      maxAge: 60 * 60 * 1, // 1 hour
+    });
+
+    api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+    setUser(user);
+
+    Router.push("/dashboard");
+  }
+
+  return (
+    <SideContext.Provider value={{ user, isAuthenticated, signIn }}>
+      {children}
+    </SideContext.Provider>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,52 @@
 import { FaFacebook, FaGooglePlus, FaTwitter } from "react-icons/fa";
-
+import { useForm } from "react-hook-form";
+import { useContext } from "react";
+import { SideContext } from "../contexts/SideBarContext";
 const Login: React.FC<{}> = () => {
+  const { signIn } = useContext(SideContext);
+  const { register, handleSubmit } = useForm();
+  async function onSubmit(data) {
+    signIn(data);
+  }
   return (
     <>
       <div className="container">
         <div className="content">
           <div className="title">Login</div>
-          <input type="text" placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
-          <input type="checkbox" id="rememberMe" />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("email")} type="text" placeholder="E-mail" />
+            <input
+              {...register("password")}
+              type="password"
+              placeholder="Senha"
+            />
+            <input type="checkbox" id="rememberMe" />
 
-          <button>Entrar</button>
-          <div className="social">
-            <span>Ou entre com</span>
-          </div>
-          <div className="buttons">
-            <button className="facebook">
-              <i className="fa ">
-                <FaFacebook />
-              </i>
-              Facebook
-            </button>
-            <button className="twitter">
-              <i className="fa">
-                <FaTwitter />
-              </i>
-              Twitter
-            </button>
-            <button className="google">
-              <i className="fa">
-                <FaGooglePlus />
-              </i>
-              Google
-            </button>
-          </div>
+            <button>Entrar</button>
+            <div className="social">
+              <span>Ou entre com</span>
+            </div>
+            <div className="buttons">
+              <button className="facebook">
+                <i className="fa ">
+                  <FaFacebook />
+                </i>
+                Facebook
+              </button>
+              <button className="twitter">
+                <i className="fa">
+                  <FaTwitter />
+                </i>
+                Twitter
+              </button>
+              <button className="google">
+                <i className="fa">
+                  <FaGooglePlus />
+                </i>
+                Google
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
