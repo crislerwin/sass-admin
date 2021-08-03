0d925733d667b099a119b76fe7dc2b45572bb4f9
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-03 20:33:54 -0300

    🎨 edit swr fetch hook file

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,10 +1,14 @@
 import useSWR from "swr";
-import api from "../services/api";
+import axios from "axios";
+
+const base = axios.create({
+  baseURL: "https://api.github.com",
+});
 export function useFetch<Data = any, Error = any>(url: string) {
   const { data, error } = useSWR<Data, Error>(
     url,
     async (url) => {
-      const response = await api.get(url);
+      const response = await base.get(url);
 
       return response.data;
     },
