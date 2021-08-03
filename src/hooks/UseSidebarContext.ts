ed3012b3c4cceb9600444209d5339655892e829d
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-03 20:34:23 -0300

    🔥 delete unnecessary component

--- src/hooks/UseSidebarContext.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import { SideBarContext } from "../contexts/SideBarContext";
-
-export const useSideBarContext = () => {
-  const context = useContext(SideBarContext);
-  const { item, setItem } = context;
-  return { item, setItem };
-};
